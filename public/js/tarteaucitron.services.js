d43b1ffe6680f0f5ff3fc988ea4e2d2d13d0ea9f
Optam00 <matthieupolaina@Host-002.lan>
2025-04-02 00:37:38 +0200

    feat: Réintégration de tarteaucitron avec Google Consent Mode

---
+++ public/js/tarteaucitron.services.js
@@ -0,0 +1,16 @@
+tarteaucitron.services.gtag = {
+    "key": "gtag",
+    "type": "analytic",
+    "name": "Google Analytics",
+    "uri": "https://policies.google.com/privacy",
+    "needConsent": true,
+    "cookies": ['_ga', '_gat', '_gid', '__utma', '__utmb', '__utmc', '__utmt', '__utmz'],
+    "js": function () {
+        window.dataLayer = window.dataLayer || [];
+        tarteaucitron.addScript('https://www.googletagmanager.com/gtag/js?id=G-7QV1MCQ879', '', function () {
+            window.gtag = function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7QV1MCQ879', { 'anonymize_ip': true });
+        });
+    }
+}; 
