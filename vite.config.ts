8891fc29b5cd94ecfc45a52995942740851db806
Optam00 <matthieupolaina@Host-002.lan>
2025-04-04 21:01:01 +0200

    fix: retour à des versions stables et compatibles Node.js 18

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    target: 'es2018',
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2018'
+    }
   }
 }) 
