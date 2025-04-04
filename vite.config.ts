984c3949fca6154f171f388981e8d9081ff8d3a6
Optam00 <matthieupolaina@Host-002.lan>
2025-04-04 07:11:48 +0200

    fix: downgrade des versions pour meilleure compatibilité

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-    sourcemap: true,
-    copyPublicDir: true
-  },
-  publicDir: 'public'
+    outDir: 'dist'
+  }
 }) 
