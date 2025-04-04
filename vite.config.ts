fa2f1704f785159e968259df75d4ae711402cc90
Optam00 <matthieupolaina@Host-002.lan>
2025-04-04 20:57:12 +0200

    fix: mise à jour majeure de Vite et simplification de la configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    minify: 'terser',
-    sourcemap: true,
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  },
-  server: {
-    port: 3000
+    outDir: 'dist'
   }
 }) 
