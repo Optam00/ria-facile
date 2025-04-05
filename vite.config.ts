63b751618037f5326a402a556a7966288b6e788b
Optam00 <matthieupolaina@Host-002.lan>
2025-04-05 15:29:29 +0200

    fix: ajout du plugin vite-plugin-commonjs pour gérer les modules CommonJS

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCommonjs()
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@supabase/postgrest-js': '@supabase/postgrest-js/dist/module/index.js'
+      '@': path.resolve(__dirname, './src')
     }
   },
   build: {
     sourcemap: true,
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    },
     rollupOptions: {
       output: {
         manualChunks: {
